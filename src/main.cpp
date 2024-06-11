3b49d64dddecf029fe4d6c54520df44c4bb3e42b
Jack Crowley <jcrowley.jjc@gmail.com>
2024-06-11 19:05:35 -0400

    renderer changes

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,16 @@ static void glfw_error_callback(int error, const char* description)
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
 
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        printf( "Mouse click at (%f, %f)\n", xpos, ypos);
+    }
+}
+
 
 
 int main(int, char**)
@@ -41,6 +51,7 @@ int main(int, char**)
         return 1;
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1); 
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
@@ -64,7 +75,7 @@ int main(int, char**)
 
     bool show_demo_window = true;
     bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    ImVec4 clear_color = ImVec4(0, 0, 0, 1.00f);
    
 
     std::vector<Circle> circles{
@@ -73,6 +84,7 @@ int main(int, char**)
         Circle(Component(5,-5), Color(255,255,255)),
         Circle(Component(-5,5), Color(255,255,255))
         };
+
 
     while (!glfwWindowShouldClose(window))
     {
@@ -84,39 +96,22 @@ int main(int, char**)
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-         static float f = 10.0f;
-
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        static float size = 10.0f;
+        static float scale = 10.0f;
+        static bool showGrid = false;
 
         {
-            
-            static int counter = 0;
+            ImGui::Begin("Particle Simulator!");
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+            ImGui::Text("This is some useful text.");
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
+            ImGui::SliderFloat("Scale", &scale, 10.0f, 250.0f);      
+            ImGui::SliderFloat("Size", &size, 1.0f, 250.0f);      
 
-            ImGui::SliderFloat("float", &f, 10.0f, 250.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+            ImGui::Checkbox("Show Grid", &showGrid);
 
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
             ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
-        }
-
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
             ImGui::End();
         }
 
@@ -126,7 +121,11 @@ int main(int, char**)
         glViewport(0, 0, display_w, display_h);
         glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
-        drawCircles(window, circles, f);
+        if (showGrid) {
+            drawGrid(window, size);
+        }
+
+        drawCircles(window, circles, scale);
 
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
--- src/renderer.cpp
+++ src/renderer.cpp
@@ -4,6 +4,19 @@
 #include <cmath>
 #include "stdio.h"
 
+
+float getNormalizedX(float x) {
+    return x/640.0;
+}
+
+float getNormalizedY(float y) {
+    return y/360.0;
+}
+
+float getNormalizedRadius(float r) {
+    return r/720.0;
+}
+
 void drawCircles(GLFWwindow* window, std::vector<Circle> circles, float &scaleFactor) {
     int width, height;
     glfwGetWindowSize(window, &width, &height);
@@ -15,16 +28,29 @@ void drawCircles(GLFWwindow* window, std::vector<Circle> circles, float &scaleFa
     for (Circle circ : circles) {
         glBegin(GL_LINE_LOOP);
         glColor3f(1.0, 1.0, 1.0);
-        double radius = circ.radius/scalingFactor;
-        double x, y;
+        float radius = getNormalizedRadius(circ.radius);
         for (int i = 0; i < 360; i++) {
-            // x = aspectRatio * radius * cos(i * 3.14159 / 180.0) + circ.position.x/scalingFactor;
-
-            // Works without aspectRatio scaling atm
-            x = aspectRatio * (radius * cos(i * 3.14159 / 180.0) + circ.position.x/scalingFactor);
-            y = radius * sin(i * 3.14159 / 180.0) + circ.position.y/scalingFactor;
+            float x = aspectRatio * (radius * cos(i * 3.14159 / 180.0) + getNormalizedX(circ.position.x));
+            float y = radius * sin(i * 3.14159 / 180.0) + getNormalizedY(circ.position.y);
             glVertex2f(x, y);
         }
         glEnd();
     }
+}
+
+void drawGrid(GLFWwindow* window, int size) {
+    glBegin(GL_LINES);
+    for (int i = -640; i < 640; i+=size) {
+        glBegin(GL_LINES);
+        glVertex2f(getNormalizedX(i), -1.0f);
+        glVertex2f(getNormalizedX(i), 1.0f);
+        glEnd();
+    }
+    for (int i = -360; i < 360; i+=size) {
+        glBegin(GL_LINES);
+        glVertex2f(-1.0f, getNormalizedY(i));
+        glVertex2f(1.0f, getNormalizedY(i));
+        glEnd();
+    }
+    glEnd();
 }
